b95e79e29a083ea3f6604374c08586b352b450b1
wonnho <wonnho71@naver.com>
2023-02-11 15:26:32 +0900

    Router
    searchParams children

--- movie_list/src/App.js
+++ movie_list/src/App.js
@@ -1,20 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./components/practice/About";
-import Home from "./components/practice/Router";
+import Home from "./components/practice/Home";
 import Headers from "./components/practice/Header";
 //import Todos from "./components/practice/Todos";
 
 function App() {
   return (
     <>
-    <Headers />
-    
-    <Routes>
-      {/* Rpites와 Route를 통해서 주소에 따라 보여줄 컴포넌트를 나눈다. */}
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
+      <Headers />
+
+      <Routes>
+        {/* Routes와 Route를 통해서 주소에 따라 보여줄 컴포넌트를 나눈다. */}
+        <Route path="/" element={<Home />}>
+          <Route path=":id" element={<div> 나타납니다.</div>} />
+        </Route>
+        <Route path="about/*" element={<About />} />
+      </Routes>
     </>
   );
 }
--- movie_list/src/components/practice/Detail.js
+++ movie_list/src/components/practice/Detail.js
@@ -1,10 +1,33 @@
+import { useParams, useSearchParams } from "react-router-dom";
+
+const list = [
+  { id: 1, text: "게시글" },
+  { id: 2, text: "리액트" },
+  { id: 3, text: "액시오스" },
+  { id: 4, text: "REST API" },
+];
 function Detail() {
-    return (
-        <div>
-            <h1>Detail</h1>
-        </div>
-    )
+  // const params=useParams();
+  const { id } = useParams(); //path parameter를 가져올 수 있다. 문자열로 넘어온다.
+  // console.log(params);
+
+  const [searchParams] = useSearchParams();
+
+  const post = list.find((item) => item.id === parseInt(id));
+
+  console.log(searchParams.get("name"));
+
+  //반복 가능한 객체
+  for (let key of searchParams.keys()) {
+    console.log(key);
+  }
+
+  return (
+    <div>
+      <h2>상세보기 {id}</h2>
+      {post.text}
+    </div>
+  );
 }
 
 export default Detail;
-
